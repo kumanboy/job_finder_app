70bc414ca684b0157c9c225f737d4e898e6cd3a0
kumanboy <husandavronov13@gmail.com>
2023-07-31 11:53:55 +0500

    HomeScreen End

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,30 @@
-'use strict';
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
 
-import {Stack} from 'expo-router';
+// SplashScreen.preventAutoHideAsync();
 
-const Layout= () => {
-    return <Stack />;
-}
+export const unstable_settings = {
+    // Ensure any route can link back to `/`
+    initialRouteName: "home",
+};
+
+const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+      <Stack initialRouteName="home">
+          <Stack.Screen name="home" />
+      </Stack>
+    )
+};
 
 export default Layout;
